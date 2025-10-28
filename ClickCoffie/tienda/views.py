887afd610615d8a6ed70a0049a9f945c75e53b57
LuzTorres20 <luz.garciat@campusucc.edu.co>
2025-10-27 19:35:02 -0500

    registro usuario

--- ClickCoffie/tienda/views.py
+++ ClickCoffie/tienda/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegistroUsuarioForm
+from django.contrib import messages
 # Create your views here.
+
+def registrar_usuario(request):
+    if request.method == "POST":
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request= "El usuario fue registrado correctamente")
+            return redirect('Login')
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, "templates/formularios/RegistroUsuarios.html", {"form": form})
+    
